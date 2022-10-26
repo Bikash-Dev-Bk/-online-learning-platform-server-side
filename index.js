d4f989b547fa8da27ca57bd6ef69f1eb25b5c29d
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-27 04:43:21 +0600

    some modify

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Course API running');
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find(course => course.id === id);
+  res.send(selectedCourses);
 });
 
 app.listen(port, () => {
